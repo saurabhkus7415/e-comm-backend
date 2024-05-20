d11da37a99c28971828762ba6be14e588cab3dfc
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-05-20 17:40:09 +0530

    integrated login and register api

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/user");
 const app = express();
-app.get("/", (req, resp) => {
-  resp.send("app is working");
-}),
-  app.listen(3000);
+app.use(cors()); //for resolved using as a like  middleware
+app.use(express.json());
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  // for added line to remove password
+  result = result.toObject();
+  delete result.password;
+  resp.send(result);
+});
+app.post("/login", async (req, resp) => {
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "User not Found" });
+    }
+  } else {
+    resp.send({ result: "email and password are required" });
+  }
+});
+
+app.listen(3000);
